a62dea78b2bc04e250088aaea95d94059636ee69
Efthimis <efthimismitk48@gmail.com>
2024-04-09 15:18:59 +0300

    asd

--- src/PHASE_A/Main.java
+++ src/PHASE_A/Main.java
@@ -3,7 +3,7 @@ import java.io.File;
 
 public class Main {
     // edit this
-    static String path = "G:\\hy463\\MedicalCollection";
+    static String path = "/home/efthimis/Downloads/HY463/MedicalCollection/Test";
     public static void main(String[] args) {
         File folder = new File(path);
         Analysis A = new Analysis(folder);
