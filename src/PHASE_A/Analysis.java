f7538b253dc9447563bda6b39952e4fc7602ff0a
Efthimis <efthimismitk48@gmail.com>
2024-04-09 17:14:31 +0300

    asd

--- src/PHASE_A/Analysis.java
+++ src/PHASE_A/Analysis.java
@@ -1,6 +1,15 @@
 package PHASE_A;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+
+import static kotlin.test.AssertionsKt.assertEquals;
 
 public class Analysis {
 
@@ -9,13 +18,43 @@ public class Analysis {
     }
 
     private static void listFilesForFolder(File folder) {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        }
         for (File fileEntry : folder.listFiles()) {
             if (fileEntry.isDirectory()) {
                 listFilesForFolder(fileEntry);
             } else {
-                System.out.println(fileEntry.getAbsolutePath());
-                File file = new File(fileEntry.getAbsolutePath());
-                // Process the file
+                try {
+                    File file = new File(fileEntry.getAbsolutePath());
+                    System.out.println(file);
+                    DocumentBuilder db = dbf.newDocumentBuilder();
+                    Document doc = db.parse(file);
+                    doc.getDocumentElement().normalize();
+
+                    // Retrieve subj-group elements
+                    NodeList subjGroupList = doc.getElementsByTagName("article");
+                    for (int i = 0; i < subjGroupList.getLength(); i++) {
+                        Node subjGroupNode = subjGroupList.item(i);
+                        if (subjGroupNode.getNodeType() == Node.ELEMENT_NODE) {
+                            Element subjGroupElement = (Element) subjGroupNode;
+
+                            // Retrieve subject elements under each subj-group
+                            NodeList subjectList = subjGroupElement.getElementsByTagName("subject");
+
+                            // Print information about each subject
+                            for (int j = 0; j < subjectList.getLength(); j++) {
+                                Node subjectNode = subjectList.item(j);
+                                System.out.println("Subject: " + subjectNode.getTextContent());
+                            }
+                        }
+                    }
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
 
             }
         }
