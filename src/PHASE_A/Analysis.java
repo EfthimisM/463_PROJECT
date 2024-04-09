5305c272628eb4446ec6fba345f4be3d60c7b2a1
EfthimisM <110114547+EfthimisM@users.noreply.github.com>
2024-04-10 01:13:00 +0300

    Αλλαξα τη main ωστε να μην πρεπει να αλλαζουμε συνεχια το path. Απλα το περναμε απο το command line: πχ   java Main C:\Test_mikro\Test
    -Ekana ta tags, vasika o tzitzikas ta ekane eixe etoimi bibliothiki stin ekfonisi tis 2is faseis
    -Ekana article class pou pros to paron apla dexetai ta 8 tags os orismata

--- src/PHASE_A/Analysis.java
+++ src/PHASE_A/Analysis.java
@@ -1,24 +1,42 @@
 package PHASE_A;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import gr.uoc.csd.hy463.NXMLFileReader;
 
 public class Analysis {
 
+    private class Article {
+        int pmcId;
+        String title;
+        String abstr;
+        String body;
+        String journal;
+        String publisher;
+        ArrayList<String> authors;
+        HashSet<String> categories;
+
+        public Article(int id, String tt, String ab, String bod, String j, String pub, ArrayList<String> auth, HashSet<String> cat){
+            pmcId = id;
+            title = tt;
+            abstr = ab;
+            body = bod;
+            journal = j;
+            publisher = pub;
+            authors = auth;
+            categories = cat;
+        }
+    }
+
     public Analysis(File folder){
         listFilesForFolder(folder);
     }
 
-    private static void listFilesForFolder(File folder) {
+    private  void listFilesForFolder(File folder) {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         try {
             dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
@@ -50,77 +68,9 @@ public class Analysis {
                     System.out.println("- Publisher: " + publisher);
                     System.out.println("- Authors: " + authors);
                     System.out.println("- Categories: " + categories);
-                    /*DocumentBuilder db = dbf.newDocumentBuilder();
-                    Document doc = db.parse(file);
-                    doc.getDocumentElement().normalize();
 
-                    // Retrieve article elements
-                    NodeList subjGroupList = doc.getElementsByTagName("article");
-                    for (int i = 0; i < subjGroupList.getLength(); i++) {
-                        Node subjGroupNode = subjGroupList.item(i);
-                        if (subjGroupNode.getNodeType() == Node.ELEMENT_NODE) {
-                            Element subjGroupElement = (Element) subjGroupNode;
+                    Article article = new Article(Integer.parseInt(pmcid), title, abstr, body, journal, publisher, authors, categories);
 
-                            // Retrieve subject elements under each article
-                            NodeList Titles = subjGroupElement.getElementsByTagName("title-group");
-
-                            // Print information about each article
-                            for (int j = 0; j < Titles.getLength(); j++) {
-                                Node subjectNode = Titles.item(j);
-                                Element sub = (Element) subjectNode;
-                                System.out.println("\tTitle: " + subjectNode.getTextContent());
-                            }
-
-                            // Retrieve subject elements under each article
-                            NodeList NamesList = subjGroupElement.getElementsByTagName("publisher-name");
-
-                            // Print information about each article
-                            for (int j = 0; j < NamesList.getLength(); j++) {
-                                Node subjectNode = NamesList.item(j);
-                                System.out.println("\tPublisher Name: " + subjectNode.getTextContent());
-                            }
-
-                            NodeList PubList = subjGroupElement.getElementsByTagName("article-id");
-                            for (int j = 0; j < PubList.getLength(); j++) {
-                                Node subjectNode = PubList.item(j);
-                                Element sub = (Element) subjectNode;
-                                if(sub.getAttribute("pub-id-type").equals("pmc")) {
-                                    System.out.println("\tPUB Name: " + subjectNode.getTextContent());
-                                }
-                            }
-
-                            // Retrieve Authors
-                            NodeList contribList = subjGroupElement.getElementsByTagName("contrib");
-                            for (int j = 0; j < contribList.getLength(); j++) {
-                                Element contribElement = (Element) contribList.item(j);
-                                String contribType = contribElement.getAttribute("contrib-type");
-
-                                if (contribType.equals("author")) {
-                                    Element nameElement = (Element) contribElement.getElementsByTagName("name").item(0);
-                                    String surname = nameElement.getElementsByTagName("surname").item(0).getTextContent();
-                                    String givenNames = nameElement.getElementsByTagName("given-names").item(0).getTextContent();
-
-                                    System.out.println("\tAuthor: " + surname + " " + givenNames);
-                                }
-                            }
-
-                            NodeList Categories = subjGroupElement.getElementsByTagName("article-categories");
-                            for (int j = 0; j < Categories.getLength(); j++) {
-                                Node subjectNode = Categories.item(j);
-                                System.out.println("\tCategory: " + subjectNode.getTextContent());
-                            }
-
-                            NodeList Journal = subjGroupElement.getElementsByTagName("journal-title");
-                            for (int j = 0; j < Journal.getLength(); j++) {
-                                Node subjectNode = Journal.item(j);
-                                System.out.println("\tJournal: " + subjectNode.getTextContent());
-                            }
-
-                            // Missing: περιληψη και κυριος περιεγχομενο
-
-                        }
-
-                    }*/
                 } catch (Exception e){
                     e.printStackTrace();
                 }
