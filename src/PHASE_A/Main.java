2990fbdecaa64af00c997c8d3fdf7d833c57b8ac
marselo <senkamarselo@gmail.com>
2024-04-09 15:22:12 +0300

    testtt

--- src/PHASE_A/Main.java
+++ src/PHASE_A/Main.java
@@ -3,7 +3,7 @@ import java.io.File;
 
 public class Main {
     // edit this
-    static String path = "G:\\hy463\\MedicalCollection";
+    static String path = "heyyyyyyyyyy";
     public static void main(String[] args) {
         File folder = new File(path);
         Analysis A = new Analysis(folder);
