9c54b64435b41d5672783d17afe1f4453f0aeff9
marselosenka <71778825+marselosenka@users.noreply.github.com>
2024-04-25 19:34:28 +0300

    Update Article.java

--- src/PHASE_A/Article.java
+++ src/PHASE_A/Article.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package PHASE_A;
 
 import java.util.ArrayList;
@@ -25,139 +20,112 @@ public class Article {
     ArrayList<String> publisherTokenized;
     ArrayList<String> authors;
     HashSet<String> categories;
-    Map<String, Map<String, ArrayList<Integer>>> vocabulary;
-    ArrayList<Word> vocab = new ArrayList();
+    Map<String,Map<String,ArrayList<Integer>>> vocabulary;
+    ArrayList<Word> vocab = new ArrayList<>();
     private int maxFrequency;
     private String maxFrequencyTerm;
     String path;
 
-    public Article(String id, String tt, String ab, String bod, String j, String pub, ArrayList<String> auth, HashSet<String> cat, String path) {
-        this.pmcId = id;
-        this.title = tt;
-        this.abstr = ab;
-        this.body = bod;
-        this.journal = j;
-        this.publisher = pub;
-        this.authors = auth;
-        this.categories = cat;
+    public Article(String id, String tt, String ab, String bod, String j, String pub, ArrayList<String> auth, HashSet<String> cat, String path){
+        pmcId = id;
+        title = tt;
+        abstr = ab;
+        body = bod;
+        journal = j;
+        publisher = pub;
+        authors = auth;
+        categories = cat;
         this.path = path;
     }
 
+    // Function that removes punctuation and removes stop words
     public static String tokenize(String word, List<String> tokens) {
-        char[] punctuation = new char[]{'.', ',', '?', '!', ';', ':', '\'', '"', ')', ']', '}', '(', '[', '{', '<', '>', '/', '\\', '-', '=', '+', '*', '~', '\t', '|', '^', '_', '#', '$', '%', '&', '\n'};
+        char[] punctuation = {'.', ',', '?', '!', ';', ':', '\'', '\"', ')', ']', '}', '(', '[', '{', '<', '>', '/', '\\', '-', '=', '+', '*', '~', '\t','|','^','_','#','$','%','&','\n'};
         if (tokens.contains(word)) {
             return "A";
-        } else {
-            char[] var3 = punctuation;
-            int var4 = punctuation.length;
-
-            for(int var5 = 0; var5 < var4; ++var5) {
-                char p = var3[var5];
-                if (word.indexOf(p) != -1) {
-                    String tmp = word.replace(Character.toString(p), "");
-                    word = tokenize(tmp, tokens);
-                }
-            }
-
-            if (word.isEmpty()) {
-                return "A";
-            } else {
-                return word;
+        }
+        for (char p : punctuation) {
+            if (word.indexOf(p) != -1) {
+                String tmp = word.replace(Character.toString(p), "");
+                word = tokenize(tmp, tokens);
             }
         }
+        if(word.isEmpty()){
+            return "A";
+        }
+        return word;
     }
 
-    public void tokenize(List<String> stopWords) {
-        this.titleTokenized = new ArrayList();
-        this.abstrTokenized = new ArrayList();
-        this.bodyTokenized = new ArrayList();
-        this.journalTokenized = new ArrayList();
-        this.publisherTokenized = new ArrayList();
+    public void tokenize(List<String> stopWords){
+
+        titleTokenized = new ArrayList<>();
+        abstrTokenized = new ArrayList<>();
+        bodyTokenized = new ArrayList<>();
+        journalTokenized = new ArrayList<>();
+        publisherTokenized = new ArrayList<>();
+
         int rank = 0;
-        String[] words = this.title.split(" ");
-        String[] var4 = words;
-        int var5 = words.length;
-
-        int var6;
-        String word;
-        String tmp;
-        for(var6 = 0; var6 < var5; ++var6) {
-            word = var4[var6];
-            ++rank;
-            tmp = tokenize(word, stopWords);
-            if (!tmp.endsWith("A")) {
-                this.titleTokenized.add(Stemmer.Stem(tmp));
+        //title
+        String[] words = title.split(" ");
+        for(String word: words){
+            rank ++;
+            String tmp = tokenize(word, stopWords);
+            if(!tmp.endsWith("A")){
+                titleTokenized.add(Stemmer.Stem(tmp));
+            }
+        }
+        //abs
+        words = abstr.split(" ");
+        for(String word: words){
+            rank ++;
+            String tmp = tokenize(word, stopWords);
+            if(!tmp.endsWith("A")){
+                abstrTokenized.add(Stemmer.Stem(tmp));
+            }
+        }
+        //body
+        words = body.split(" ");
+        for(String word: words){
+            rank ++;
+            String tmp = tokenize(word, stopWords);
+            if(!tmp.endsWith("A")){
+                bodyTokenized.add(Stemmer.Stem(tmp));
             }
         }
 
-        words = this.abstr.split(" ");
-        var4 = words;
-        var5 = words.length;
-
-        for(var6 = 0; var6 < var5; ++var6) {
-            word = var4[var6];
-            ++rank;
-            tmp = tokenize(word, stopWords);
-            if (!tmp.endsWith("A")) {
-                this.abstrTokenized.add(Stemmer.Stem(tmp));
+        words = journal.split(" ");
+        for(String word: words){
+            rank ++;
+            String tmp = tokenize(word, stopWords);
+            if(!tmp.endsWith("A")){
+                journalTokenized.add(Stemmer.Stem(tmp));
             }
         }
 
-        words = this.body.split(" ");
-        var4 = words;
-        var5 = words.length;
-
-        for(var6 = 0; var6 < var5; ++var6) {
-            word = var4[var6];
-            ++rank;
-            tmp = tokenize(word, stopWords);
-            if (!tmp.endsWith("A")) {
-                this.bodyTokenized.add(Stemmer.Stem(tmp));
-            }
-        }
-
-        words = this.journal.split(" ");
-        var4 = words;
-        var5 = words.length;
-
-        for(var6 = 0; var6 < var5; ++var6) {
-            word = var4[var6];
-            ++rank;
-            tmp = tokenize(word, stopWords);
-            if (!tmp.endsWith("A")) {
-                this.journalTokenized.add(Stemmer.Stem(tmp));
-            }
-        }
-
-        words = this.publisher.split(" ");
-        var4 = words;
-        var5 = words.length;
-
-        for(var6 = 0; var6 < var5; ++var6) {
-            word = var4[var6];
-            ++rank;
-            tmp = tokenize(word, stopWords);
-            if (!tmp.endsWith("A")) {
-                this.publisherTokenized.add(Stemmer.Stem(tmp));
+        words = publisher.split(" ");
+        for(String word: words){
+            rank ++;
+            String tmp = tokenize(word, stopWords);
+            if(!tmp.endsWith("A")){
+                publisherTokenized.add(Stemmer.Stem(tmp));
             }
         }
 
     }
-
-    public void setVocabulary(Map<String, Map<String, ArrayList<Integer>>> vocabulary) {
+    public void setVocabulary(Map<String,Map<String,ArrayList<Integer>>> vocabulary) {
         this.vocabulary = vocabulary;
     }
 
-    public Map<String, Map<String, ArrayList<Integer>>> getVocabulary() {
-        return this.vocabulary;
+    public Map<String,Map<String,ArrayList<Integer>>> getVocabulary() {
+        return vocabulary;
     }
 
     public int getMaxFrequency() {
-        return this.maxFrequency;
+        return maxFrequency;
     }
 
     public String getMaxFrequencyTerm() {
-        return this.maxFrequencyTerm;
+        return maxFrequencyTerm;
     }
 
     public void setMaxFrequency(int maxFrequency) {
